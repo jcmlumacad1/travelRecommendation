8611f7677245c52549f273d12e2e21904d5d3323
jcmlumacad1 <44424424+jcmlumacad1@users.noreply.github.com>
2024-09-06 16:16:23 +0800

    create and integrate travel search

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,5 +1,25 @@
-async function search() {
-  fetch("travel_recommendation_api.json")
-    .then((response) => response.json())
-    .then(console.log);
+document.querySelector("form").addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  console.log("search(formData)", await search(formData.get("query")));
+});
+
+const keywords = {
+  beach: "beaches",
+  beaches: "beaches",
+  temple: "temples",
+  temples: "temples",
+  country: "countries",
+  countries: "countries",
+};
+
+async function search(query) {
+  const keyword = keywords[query.toLowerCase()];
+  if (!keyword) return [];
+
+  const data = await fetch("travel_recommendation_api.json").then((response) =>
+    response.json()
+  );
+
+  return data[keyword];
 }
